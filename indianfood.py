060a5eb64a60a3c04991bb99fa8fc139f11a3df2
VictorJuang <r05943050@ntu.edu.tw>
2020-03-01 13:30:08 -0600

    add indian food

---
+++ indianfood.py
@@ -0,0 +1,38 @@
+INDIAN_FOOD ={
+    'style_check':[
+        'india',
+        'balti',
+        'garam',
+        'masala',
+        'turmeric',
+        'murgh',
+        'makhani',
+        'chapati',
+        'lentil',
+        'barley',
+        'pilaf',
+        'korma',
+        'madras'
+    ],
+    'change_dict':{
+        'pork': ['chicken', 'lamb', 'fish fillets'],
+        'beef': ['chicken', 'lamb', 'fish fillets'],
+        'rice': ['basmati rice', 'chickpeas', 'lentil'],
+        'bread': ['chapati bread'],
+        'toast': ['chapati bread'],
+        'curry powder': ['madras curry powder'],
+        'milk': ['coconut milk'],
+        'tea powder': ['chai tea bag'],
+        'tea leaves': ['chai tea bag']
+    },
+    'add_item_method': ['mix', 'stir', 'blend', 'combine'],
+    'add_item_ingred':[
+        ['1/4', 'cup', 'coconut milk'],
+        ['1', 'teaspoon', 'garam masala'],
+        ['1', 'teaspoon', 'madras curry powder'],
+        ['1', 'teaspoon', 'ground turmeric'],
+        ['1/4', 'teaspoon', 'indian black salt']
+        
+    ],
+    'primary_methods': ['broil', 'fry', 'simmer', 'grill', 'bake']
+} 
