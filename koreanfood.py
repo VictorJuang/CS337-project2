6d9cda154facfa0033022da575d43fa8351543fd
Alex Tang <alextang@u.northwestern.edu>
2020-03-01 17:23:12 -0600

    korean & mexico

---
+++ koreanfood.py
@@ -0,0 +1,43 @@
+KOREAN_FOOD ={
+    'style_check':[
+        'korea',
+        'korean',
+        'bulgogi',
+        'kalbi',
+        'kimchi',
+        'bibimbap',
+        'chee',
+        'kongnamool',
+        'galbi',
+        'bi',
+        'bim',
+        'bap',
+        'jun',
+        'chae',
+        'japchae',
+        'kimbop',
+        'toasti',
+        'deopbap',
+        'chompchae',
+        'mandu',
+        'gochujang'
+    ],
+    'change_dict':{
+        'bread': ['rice'],
+        'toast': ['rice'],
+        'curry powder': ['korean chili pepper'],
+        'cabbage' : ['kimchi'],
+        'spinach' : ['kimchi'],
+        'lettuce' : ['kimchi'],
+        'sause' : ['korean spicy sauce'],
+        'ketchup' : ['korean spicy sauce'],
+        'oil' : ['sesame oil']
+    },
+    'add_item_method': ['mix', 'stir', 'blend', 'combine'],
+    'add_item_ingred':[
+        ['1/4', 'teaspoon', 'sesame oil'],
+        ['1', 'teaspoon', 'korean chili pepper'],
+        ['1', 'teaspoon', 'korean spicy sause'],
+    ],
+    'primary_methods': ['broil', 'fry', 'simmer', 'grill']
+} 
---
+++ mexicanfood.py
@@ -0,0 +1,39 @@
+INDIAN_FOOD ={
+    'style_check':[
+        'mexican',
+        'barbacoa',
+        'carnitas',
+        'salsa',
+        'flauta',
+        'taco',
+        'jalapeno',
+        'guacamole',
+        'tortilla',
+        'enchiladas',
+        'burrito',
+        'sopapilla',
+        'quesadillas',
+        'leches',
+        'chipotle',
+        'fajita',
+        'chimichangas',
+        'chili',
+        'nacho',
+        'churro'
+    ],
+    'change_dict':{
+        'bread': ['taco', 'burrito', 'corn bread'],
+        'toast': ['taco', 'burrito', 'corn bread'],
+        'curry powder': ['jalapeno', 'chili powder'],
+        'egg' : ['avocado'],
+        'bean' : ['black bean']
+    },
+    'add_item_method': ['mix', 'stir', 'blend', 'combine'],
+    'add_item_ingred':[
+        ['1', 'teaspoon', 'lime juice'],
+        ['1/2', 'cup', 'cilantro'],
+        ['1', 'teaspoon', 'chili powder'],
+        
+    ],
+    'primary_methods': ['fry', 'simmer', 'grill', 'bake']
+} 
